878dad8924a4a01b4b4d8830b613d0b9e1f9e93c
RaresB <i414572@fhict.nl>
2020-04-12 21:03:24 +0200

    Updated header + navbar

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,17 @@
 <!-- Common css links(navbar, footer) -->
-	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" type="text/css" href="css/navbar.css">
 	<link rel="stylesheet" type="text/css" href="css/footer.css">
 	</head>
 	<body>
 	<!-- Navbar -->
-	<nav>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="profile.php">Profile</a></li>
-			<li><a href="schedule.php">Schedule</a></li>
-			<li id="logout"><a href="logout.php">Log out</a></li>
-		</ul>
-	</nav>
+	<div class="navigation-bar">
+		<div id="navigation-container">
+		<img class = "logo1" src="img/logo.png">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="profile.php">Profile</a></li>
+				<li><a href="schedule.php">Schedule</a></li>
+				<li id="logout"><a href="logout.php">Log out</a></li>
+			</ul>
+		</div>
+	</div>
