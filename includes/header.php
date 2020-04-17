8834c071a1688cdcd90e1cffcae3d64626ae89d3
RaresB <i414572@fhict.nl>
2020-04-17 17:07:38 +0200

    Added respovinse design(navbar+home)

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,48 @@
 <!-- Common css links(navbar, footer) -->
 <link rel="stylesheet" type="text/css" href="css/navbar.css">
 	<link rel="stylesheet" type="text/css" href="css/footer.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<script>
+		function myFunction() {	
+ 		 	var x = document.getElementById("myLinks");
+ 	 		if (x.style.display === "block") {
+    			x.style.display = "none";
+ 			 } else {
+    			x.style.display = "block";
+  			}
+		}
+	</script>
+
 	</head>
 	<body>
 	<!-- Navbar -->
-	<div class="navigation-bar">
-		<div id="navigation-container">
-		<img class = "logo1" src="img/logo.png">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="info.php">Profile</a></li>
-				<li><a href="monthly_calendar.php">Schedule</a></li>
-				<li id="logout"><a href="logout.php">Log out</a></li>
-			</ul>
+
+		<div class="navigation-bar">
+			<div id="navigation-container">
+			 <img class = "logo1" src="img/logo.png">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="info.php">Profile</a></li>
+					<li><a href="monthly_calendar.php">Schedule</a></li>
+					<li id="logout"><a href="logout.php">Log out</a></li>
+				</ul>
+			</div>
 		</div>
+
+	<div class="mobile-container">	
+		<img class = "logo2" src="img/logo.png">
+		<div class="topnav">
+			<div id="myLinks">
+					<a href="index.php">Home</a>
+					<a href="info.php">Profile</a>
+					<a href="monthly_calendar.php">Schedule</a>
+					<a href="logout.php">Log out</a>
+			</div>
+		</div>
+		<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+   				 <i class="fa fa-bars"></i>
+  			</a>
 	</div>
+
+	
--- index.php
+++ index.php
@@ -20,9 +20,10 @@
     
     
     <div class = "text">
-         <h1>Welcome to Media Bazaar!</h1>
-         <p><br></p>
-        <div class="page-wrap">
+        <p><br></p>
+        <h1>Welcome to Media Bazaar!</h1>
+        <p><br></p>
+        <div class= "page-wrap">
         
             <h2>Media Bazaar is a new hardware store which just opened in Eindhoven, funded by the parent company “Jupiter”. All kinds of good quality hardware will be sold. 
             The business will make revenue and a profit by servicing its customers not only with needed hardware but also with expert advice in the use of any product it sells.
